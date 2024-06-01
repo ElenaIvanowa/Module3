bf56adb221706f20e142da352590f0e5dd0aea03
ElenaIvanowa <167808308+ElenaIvanowa@users.noreply.github.com>
2024-06-01 22:19:53 +0300

    Модуль 1 доп

---
+++ module1hard.py
@@ -0,0 +1,18 @@
+# Исходные данные:
+
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+
+# Решение
+
+students1 = sorted(students)
+
+students_avr_grades = {}
+
+students_avr_grades[students1[0]] = sum(grades[0]) / len(grades[0])
+students_avr_grades[students1[1]] = sum(grades[1]) / len(grades[1])
+students_avr_grades[students1[2]] = sum(grades[2]) / len(grades[2])
+students_avr_grades[students1[3]] = sum(grades[3]) / len(grades[3])
+students_avr_grades[students1[4]] = sum(grades[4]) / len(grades[4])
+
+print(students_avr_grades)
